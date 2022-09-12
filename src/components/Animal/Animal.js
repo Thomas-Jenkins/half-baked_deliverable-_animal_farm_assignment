88c9e0e312ac20445f1e96a96c3e3087e8a31181
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-09-12 16:52:23 -0700

    applies CSS to rendered elements

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,10 @@
+import './Animal.css';
 export default function Animal({ name, type, says, top, left }) {
   return <>
-    <div className="animals" style={{ top, left }}></div>
-    <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
-    <span>{name}</span>
-    <span>{says}</span>
+    <div className="animal" style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span>{name}</span>
+      <span>{says}</span>
+    </div>
   </>;
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ import { animals } from '../../data';
 export default function Main() {
   return (<main style={{ backgroundImage: `url(${background})` }}>
     {animals.map((animal) => (
-      <Animal key={animal.name} {...animal}/>
+      <Animal key={animal.name} {...animal} />
     ))}
   </main>
   );
